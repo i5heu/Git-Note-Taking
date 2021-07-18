07656e9dc943e9f120e99c993bef12ddea2ec939
i5heu <i5heu@i5h.eu>
2021-07-18 20:52:30 +0200

    index for hash

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import path from "path";
 import PutBasicSetup from "./basic_setup.js";
+import { Queuing } from "./queuing.js";
 
 const directoryPath = path.join(__dirname, "../../Git-Note-Taking-Test");
 
 async function main() {
-  await new PutBasicSetup(directoryPath);
+
+  const queue = new Queuing(directoryPath);
+  await queue.prepare();
+  await queue.crateAllIndexes();
+
+  // await new PutBasicSetup(directoryPath);
 }
 
 main();
