06b18561aca6f5dd689628b789d845d34482b184
i5heu <i5heu@i5h.eu>
2021-08-23 20:28:08 +0200

    add abbility to use plugins

--- src/main.ts
+++ src/main.ts
@@ -12,16 +12,29 @@ async function main() {
   const tree = new Tree(directoryPath);
   await tree.prepare();
 
-  await tree.iterateOverTree(async (chunk) => {
-    await Hash.hashIndex(chunk);
-    await Hash.uiCreator(chunk);
+  await tree.iterateOverTree(async (mainChunk) => {
+    await Hash.hashIndex(mainChunk);
+    await Hash.uiCreator(mainChunk);
 
-    if (chunk.indexString.length > 0) {
+    if (mainChunk.indexString.length > 0) {
       fs.writeFile(
-        chunk.path + "/AA_INDEX.auto.md",
-        chunk.indexString.join(),
-        () => {}
+        mainChunk.path + "/AA_INDEX.auto.md",
+        mainChunk.indexString.join(),
+        () => { }
       );
+    }
+
+    if (mainChunk.name == ".scripts") {
+      mainChunk.subItems.forEach(script => {
+        if (!script.name.includes(".js")) return;
+        var data = fs.readFileSync(script.path, "utf8");
+        const str = data.toString();
+        try {
+          eval(str);
+        } catch (error) {
+          console.error("script.path:", script.path, "\n", error);
+        }
+      });
     }
   });
 
